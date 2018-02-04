2c06468163d736cfcfa3ef0162afed3688b2fb70
Nathan Langer <nathan@loudcrowdit.co.uk>
2018-02-04 10:33:33 +0000

    mobile nav update

--- resources/views/partials/_mobile-nav.blade.php
+++ resources/views/partials/_mobile-nav.blade.php
@@ -3,12 +3,12 @@
 *************************************-->
 <nav id="menu">
   <ul>
-    <li><a href="/">Welcome</a></li>
-    <li><a href="/">Destinations</a></li>
-    <li><a href="/">Listings</a></li>
-    <li><a href="/">Pages</a></li>
+    <li><a href="/">Home</a></li>
+    <li><a href="/">Activities</a></li>
+    <li><a href="/">News</a></li>
+    <li><a href="/">Events</a></li>
     <li><a href="/">Shop</a></li>
-    <li><a href="/">Blog</a></li>
+    <li><a href="/">Contact</a></li>
   </ul>
 </nav>
 <!--************************************
